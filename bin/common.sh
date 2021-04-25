71cd8ec868df2c926e9b9b4c0bc72b3de58a5044
Tristan Robert <tristan.robert.44@gmail.com>
2021-04-25 16:49:34 +0200

    refactor: :recycle: use mattermost env vars

--- bin/common.sh
+++ bin/common.sh
@@ -39,23 +39,6 @@ function indent() {
   esac
 }
 
-function install_jq() {
-  if [[ -f "${ENV_DIR}/JQ_VERSION" ]]; then
-    JQ_VERSION=$(cat "${ENV_DIR}/JQ_VERSION")
-  else
-    JQ_VERSION=1.6
-  fi
-  start "Fetching jq $JQ_VERSION"
-  if [ -f "${CACHE_DIR}/dist/jq-$JQ_VERSION" ]; then
-    info "File already downloaded"
-  else
-    ${CURL} -o "${CACHE_DIR}/dist/jq-$JQ_VERSION" "https://github.com/stedolan/jq/releases/download/jq-$JQ_VERSION/jq-linux64"
-  fi
-  cp "${CACHE_DIR}/dist/jq-$JQ_VERSION" "${BUILD_DIR}/bin/jq"
-  chmod +x "${BUILD_DIR}/bin/jq"
-  finished
-}
-
 function fetch_mattermost_dist() {
   local version="$1"
   local location="$2"
@@ -72,65 +55,4 @@ function fetch_mattermost_dist() {
     ${CURL} -o "${CACHE_DIR}/dist/${dist}" "${dist_url}"
   fi
   tar xzf "$CACHE_DIR/dist/${dist}" -C "$location"
-}
-
-function configure_database() {
-  local mattermost_path="$1"
-  local db_type="$2"
-  local db_user="$3"
-  local db_password="$4"
-  local db_host="$5"
-  local db_port="$6"
-  local db_name="$7"
-  local db_sslmode="$8"
-  local MM_CONFIG="${mattermost_path}/config/config.json"
-  local ENCODED_PASSWORD
-  ENCODED_PASSWORD=$(printf %s "$db_password" | jq -s -R -r @uri)
-  case $db_type in
-    postgres) 
-      jq '.SqlSettings.DriverName = "postgres"' "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-      local PG_URL="postgres://${db_user}:${ENCODED_PASSWORD}@${db_host}:${db_port}/${db_name}?sslmode=${db_sslmode}"
-      jq ".SqlSettings.DataSource = \"${PG_URL}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-      ;;
-    mysql)
-      jq '.SqlSettings.DriverName = "mysql"' "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-      local MYSQL_URL="${db_user}:${ENCODED_PASSWORD}@tcp(${db_host}:${db_port})/${db_name}"
-      jq ".SqlSettings.DataSource = \"${MYSQL_URL}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-      ;;
-    *)
-      error "${db_type} unkown. Only postgres or mysql are required."
-      ;;
-  esac
-}
-
-function configure_mattermost() {
-  local mattermost_path="$1"
-  local site_url="$2"
-  local mattermost_port="$3"
-  local smtp_host="$4"
-  local smtp_port="$5"
-  local smtp_user="$6"
-  local smtp_password="$7"
-  local file_driver_name="$8"
-  local s3_key_id="$9"
-  local s3_key_secret="${10}"
-  local s3_bucket="${11}"
-  local s3_region="${12}"
-  local s3_endpoint="${13}"
-  local MM_CONFIG="${mattermost_path}/config/config.json"
-  jq ".ServiceSettings.SiteURL = \"${site_url}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".ServiceSettings.ListenAddress = \":${mattermost_port}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SMTPUsername = \"${smtp_user}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SMTPPassword = \"${smtp_password}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SMTPServer = \"${smtp_host}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SMTPPort = \"${smtp_port}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SendPushNotifications = false" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".EmailSettings.SendEmailNotifications = true" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.DriverName = \"${file_driver_name}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.AmazonS3AccessKeyId = \"${s3_key_id}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.AmazonS3SecretAccessKey = \"${s3_key_secret}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.AmazonS3Bucket = \"${s3_bucket}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.AmazonS3Region = \"${s3_region}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".FileSettings.AmazonS3Endpoint = \"${s3_endpoint}\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
-  jq ".PluginSettings.ClientDirectory = \"/app/mattermost/client/plugins\"" "$MM_CONFIG" >"$MM_CONFIG.tmp" && mv "$MM_CONFIG.tmp" "$MM_CONFIG"
 }
